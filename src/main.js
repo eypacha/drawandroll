aecd9c577a7f4eb06e3de1f2283042b105b74662
Ey Pacha! <pachaguionbajo@gmail.com>
2026-02-05 09:49:56 -0300

    Remove PrimeVue integration from main.js and style.css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './style.css'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
-import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -12,5 +11,4 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(i18n)
 app.use(router)
-app.use(PrimeVue)
 app.mount('#app')
